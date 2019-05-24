71f2abd8d3e81b062fa5bdb47705ca2c945246a9
je941004 <je941004@naver.com>
2019-05-24 14:42:18 +0900

    if문 제거 다른 방법

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -3,6 +3,7 @@ package coordinate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class FigureFactory {
@@ -12,6 +13,15 @@ public class FigureFactory {
         map.put(3, () -> new Triangle(points));
         map.put(4, () -> new Rectangle(points));
         return map.get(points.size()).get();
+    }
+
+    //getInstance랑 같은 기능
+    private static Figure getInstatnce2(List<Point> points) {
+        Map<Integer, Function<List<Point>, Figure>> map2 = new HashMap<>();
+        map2.put(2, Line::new);
+        map2.put(2, Triangle::new);
+        map2.put(2, Rectangle::new);
+        return map2.get(points.size()).apply(points);
     }
 
 }
